a3d7ea7a45146e10386ae59fa1cf0c61aad3e8b3
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-10 19:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function incrementCounter() {
+    // Get the current counter value
+    var currentCounter = parseInt(document.getElementById('counter').innerText);
+
+    // Display an alert with the un-incremented value
+    alert("Un-incremented Value: " + currentCounter);
+
+    // Increment the counter value
+    currentCounter++;
+
+    // Update the counter value on the webpage
+    document.getElementById('counter').innerText = currentCounter;
+  }
+
